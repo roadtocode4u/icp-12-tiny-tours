33f054cdfb8f96f19f66ed4d5ced70a68c814c00
Suraj Shende <surajshende247@gmail.com>
2025-12-18 19:51:27 +0530

    User Signup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db.js";
+import User from "./models/User.js";
 
 dotenv.config();
 
@@ -18,6 +19,71 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
   res.json({ status: "OK" });
 });
+
+app.post("/signup", async (req, res) => {
+  const { name, email, mobile, city, country, password } = req.body;
+
+  if (!name) {
+    return res.json({
+      success: false,
+      message: "Name is required",
+      data: null,
+    });
+  }
+
+  if (!email) {
+    return res.json({
+      success: false,
+      message: "Email is required",
+      data: null,
+    });
+  }
+
+  if (!password) {
+    return res.json({
+      success: false,
+      message: "Password is required",
+      data: null,
+    });
+  }
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return res.json({
+      success: false,
+      message: "User with this email already exists",
+      data: null,
+    });
+  }
+
+  const newUser = new User({
+    name,
+    email,
+    mobile,
+    city,
+    country,
+    password,
+  });
+
+  try {
+    const savedUser = await newUser.save();
+
+    return res.json({
+      success: true,
+      message: "User registered successfully",
+      data: savedUser,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: `User registration failed: ${error.message}`,
+      data: null,
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
