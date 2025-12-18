9704ed5beda27d9d6f35c8c4878fca9e0c68d3ef
Suraj Shende <surajshende247@gmail.com>
2025-12-18 19:36:27 +0530

    User Schema

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", userSchema);
+
+export default User;
